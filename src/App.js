47172a379d6e558c1767c30d306ce5ef24c99e6f
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-09-17 13:48:55 +0900

    [add] router機能追加 react-router-domをインストール

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import "./styles.css";
+import React from "react";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1 className="header-top">Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <BrowserRouter>
+        <body className="App">
+          <header>
+            <h1 className="header-top">Hello CodeSandbox</h1>
+          </header>
+          <div class="openbtn1">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <nav id="g-nav">
+            <div id="g-nav-list">
+              <ul>
+                <li>
+                  <Link to="/">Top</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/service">Service</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </body>
+        <Switch>
+          <Route exact path="/"></Route>
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
